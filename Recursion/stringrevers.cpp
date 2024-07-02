6e4353d53d95ba0aa70f95840e7502d5758bcfea
ahmedyar7 <ahmedyar778@gmail.com>
2024-07-02 09:48:42 +0500

    Getting stared with the question practicing

---
+++ Recursion/stringrevers.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+void reverse_str(string s)
+{
+    if (s.length() == 0)
+    {
+        return;
+    }
+    string ros = s.substr(1);
+    reverse_str(ros);
+    cout << s[0];
+}
+
+void replace_pi(string s)
+{
+    if (s.length() == 0)
+    {
+        return;
+    }
+    if (s[0] == 'p' && s[1] == 'i')
+    {
+        cout << "3.14";
+        replace_pi(s.substr(2));
+    }
+    else
+    {
+        cout << s[0];
+        replace_pi(s.substr(1));
+    }
+}
+
+string remove_duplicate(string s)
+{
+    if (s.length() == 0) // Base case: if the string is empty
+    {
+        return ""; // Return an empty string
+    }
+    char ch = s[0];                             // Store the first character
+    string ans = remove_duplicate(s.substr(1)); // Recursive call with the rest of the string
+
+    // If the first character of the remaining string matches 'ch', skip 'ch'
+    if (ch == ans[0])
+    {
+        return ans;
+    }
+    else
+    {
+        return (ch + ans); // Otherwise, include 'ch' in the result
+    }
+}
+
+string move_all_x(string s)
+{
+    if (s.length() == 0)
+    {
+        return "";
+    }
+    char ch = s[0];
+    string ans = move_all_x(s.substr(1));
+    if (ch == 'x')
+    {
+        return ans + ch;
+    }
+    return ch + ans;
+}
+
+int main()
+{
+    // reverse_str("Ahmed");
+    // replace_pi("pippppiiiipi");
+
+    // cout << remove_duplicate("gfg");
+    cout << move_all_x("ahxmexd");
+}
