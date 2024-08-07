caad4d81c077274779de75b1258e4d2b047ba582
ahmedyar7 <ahmedyar778@gmail.com>
2024-08-07 23:52:28 +0500

    practicing the sorting

---
+++ Sorting Algorithms/bubble_sort.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void bubble_sort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+
+    int arr[] = {12, 334, 54, 23, 54, 67, 122};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    bubble_sort(arr, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
--- Sorting Algorithms/selection.cpp
+++ Sorting Algorithms/selection.cpp
@@ -1,27 +1,29 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 void selection_sort(int arr[], int n)
 {
     for (int i = 0; i < n - 1; i++)
     {
-        int min_index = i;
+        int min_idx = i;
         for (int j = i + 1; j < n; j++)
         {
-            if (arr[j] < arr[min_index])
+            if (arr[j] < arr[min_idx])
             {
-                min_index = j;
+                min_idx = j;
             }
         }
-        int temp = arr[min_index];
-        arr[min_index] = arr[i];
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
         arr[i] = temp;
     }
 }
 int main()
 {
-    int arr[] = {12, 34, 54, 23, 45, 12, 3, 43};
+    int arr[] = {122, 34, 54, 23, 45, 12, 3, 43};
     int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << n << endl;
 
     selection_sort(arr, n);
 
