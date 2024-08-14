70f554a7420a84d2fa568d7c99c3e64df8bd283d
ahmedyar7 <ahmedyar778@gmail.com>
2024-08-14 11:23:57 +0500

    adding the merge sort algorith

--- Sorting Algorithms/mergesort.cpp
+++ Sorting Algorithms/mergesort.cpp
@@ -1,62 +1,217 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void merge(int arr[], int low, int mid, int high) {
+//   vector<int> temp(high - low +
+//                    1);  // Use std::vector instead of a static array
+//   int left = low;
+//   int right = mid + 1;
+//   int k = 0;
+
+//   while (left <= mid && right <= high) {
+//     if (arr[left] <= arr[right]) {
+//       temp[k++] = arr[left++];
+//     } else {
+//       temp[k++] = arr[right++];
+//     }
+//   }
+
+//   while (left <= mid) {
+//     temp[k++] = arr[left++];
+//   }
+
+//   while (right <= high) {
+//     temp[k++] = arr[right++];
+//   }
+
+//   for (int i = 0; i < k; i++) {
+//     arr[low + i] = temp[i];
+//   }
+// }
+
+// void merge_sort(int arr[], int low, int high) {
+//   if (low < high) {
+//     int mid = (low + high) / 2;
+
+//     merge_sort(arr, low, mid);
+//     merge_sort(arr, mid + 1, high);
+
+//     merge(arr, low, mid, high);
+//   }
+// }
+
+// int main() {
+//   int arr[] = {38, 27, 43, 3, 9, 82, 10};
+//   int n = sizeof(arr) / sizeof(arr[0]);
+
+//   cout << "Original array: ";
+//   for (int i = 0; i < n; i++) {
+//     cout << arr[i] << " ";
+//   }
+//   cout << endl;
+
+//   merge_sort(arr, 0, n - 1);
+
+//   cout << "Sorted array: ";
+//   for (int i = 0; i < n; i++) {
+//     cout << arr[i] << " ";
+//   }
+//   cout << endl;
+
+//   return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void merge(int arr[], int low, int mid, int high) {
+//   vector<int> temp(high - low + 1);
+//   int left = low;
+//   int right = mid + 1;
+//   int k = 0;
+
+//   while (left <= mid && right <= high) {
+//     if (arr[left] <= arr[right]) {
+//       temp[k++] = arr[left++];
+//     } else {
+//       temp[k++] = arr[right++];
+//     }
+//   }
+
+//   while (left <= mid) {
+//     temp[k++] = arr[left++];
+//   }
+
+//   while (right <= high) {
+//     temp[k++] = arr[right++];
+//   }
+
+//   for (int i = 0; i < k; i++) {
+//     arr[low + i] = temp[i];
+//   }
+// }
+
+// void merge_sort(int arr[], int low, int high) {
+//   if (low < high) {
+//     int mid = (low + high) / 2;
+//     merge_sort(arr, low, mid);
+//     merge_sort(arr, mid + 1, high);
+//     merge(arr, low, mid, high);
+//   }
+// }
+
+// int main() {
+//   int arr[] = {13, 34, 12, 4, 45, 34, 65, 23, 1};
+//   int n = sizeof(arr) / sizeof(arr[0]);
+
+//   merge_sort(arr, 0, n - 1);
+
+//   for (int i = 0; i < n; i++) {
+//     cout << arr[i] << " ";
+//   }
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void merge(int arr[], int low, int mid, int high) {
+//   vector<int> temp(high - low + 1);
+//   int left = low;
+//   int right = mid + 1;
+//   int k = 0;
+
+//   while (left <= mid && right <= high) {
+//     if (arr[left] <= arr[right]) {
+//       temp[k++] = arr[left++];
+//     } else {
+//       temp[k++] = arr[right++];
+//     }
+//   }
+
+//   while (left <= mid) {
+//     temp[k++] = arr[left++];
+//   }
+
+//   while (right <= high) {
+//     temp[k++] = arr[right++];
+//   }
+
+//   for (int i = 0; i < k; i++) {
+//     arr[low + i] = temp[i];
+//   }
+// }
+
+// void merge_sort(int arr[], int low, int high) {
+//   if (low < high) {
+//     int mid = (low + high) / 2;
+
+//     merge_sort(arr, low, mid);
+
+//     merge_sort(arr, mid + 1, high);
+
+//     merge(arr, low, mid, high);
+//   }
+// }
+
+// int main() {
+//   int arr[] = {13, 34, 12, 4, 45, 34, 65, 23, 1};
+//   int n = sizeof(arr) / sizeof(arr[0]);
+
+//   merge_sort(arr, 0, n - 1);
+
+//   for (int i = 0; i < n; i++) {
+//     cout << arr[i] << " ";
+//   }
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
 
-void merge(int arr[], int low, int mid, int high) {
-  vector<int> temp(high - low +
-                   1);  // Use std::vector instead of a static array
+void merge(vector<int>& arr, int low, int mid, int high) {
+  vector<int> temp;
   int left = low;
   int right = mid + 1;
-  int k = 0;
 
   while (left <= mid && right <= high) {
     if (arr[left] <= arr[right]) {
-      temp[k++] = arr[left++];
+      temp.push_back(arr[left]);
+      left++;
     } else {
-      temp[k++] = arr[right++];
+      temp.push_back(arr[right]);
+      right++;
     }
   }
 
   while (left <= mid) {
-    temp[k++] = arr[left++];
+    temp.push_back(arr[left]);
+    left++;
   }
 
   while (right <= high) {
-    temp[k++] = arr[right++];
+    temp.push_back(arr[right]);
+    right++;
   }
 
-  for (int i = 0; i < k; i++) {
-    arr[low + i] = temp[i];
+  for (int i = low; i < high; i++) {
+    arr[i] = temp[i - low];
   }
 }
 
-void merge_sort(int arr[], int low, int high) {
+void mergeSort(vector<int>& arr, int low, int high) {
   if (low < high) {
     int mid = (low + high) / 2;
-
-    merge_sort(arr, low, mid);
-    merge_sort(arr, mid + 1, high);
-
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid + 1, high);
     merge(arr, low, mid, high);
   }
 }
 
 int main() {
-  int arr[] = {38, 27, 43, 3, 9, 82, 10};
-  int n = sizeof(arr) / sizeof(arr[0]);
+  vector<int> arr = {12, 34, 54, 13, 4, 5, 32, 54, 55, 22};
+  int n = arr.size();
 
-  cout << "Original array: ";
+  mergeSort(arr, 0, n - 1);
   for (int i = 0; i < n; i++) {
     cout << arr[i] << " ";
   }
-  cout << endl;
-
-  merge_sort(arr, 0, n - 1);
-
-  cout << "Sorted array: ";
-  for (int i = 0; i < n; i++) {
-    cout << arr[i] << " ";
-  }
-  cout << endl;
-
-  return 0;
 }
