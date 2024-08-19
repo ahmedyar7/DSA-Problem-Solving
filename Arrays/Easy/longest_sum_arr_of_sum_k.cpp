da2cd153376f66cdc8f8a6c6dd5e9bae9b4599ba
ahmedyar7 <ahmedyar778@gmail.com>
2024-08-19 14:13:33 +0500

    Adding another importanit file regarding the practicing

--- Arrays/Easy/longest_sum_arr_of_sum_k.cpp
+++ Arrays/Easy/longest_sum_arr_of_sum_k.cpp
@@ -18,6 +18,36 @@ class Solution {
 
     return ans;
   }
+
+  int lenOfLongSubarr_OPTIMAL_APPROACH(int arr[], int n, int k) {
+    // unordered_map 'um' implemented
+    // as hash table
+    unordered_map<int, int> um;
+    int sum = 0, maxLen = 0;
+
+    // traverse the given array
+    for (int i = 0; i < n; i++) {
+      // accumulate sum
+      sum += arr[i];
+
+      // when subarray starts from index '0'
+      if (sum == k) maxLen = i + 1;
+
+      // make an entry for 'sum' if it is
+      // not present in 'um'
+      if (um.find(sum) == um.end()) um[sum] = i;
+
+      // check if 'sum-k' is present in 'um'
+      // or not
+      if (um.find(sum - k) != um.end()) {
+        // update maxLength
+        if (maxLen < (i - um[sum - k])) maxLen = i - um[sum - k];
+      }
+    }
+
+    // required maximum length
+    return maxLen;
+  }
 };
 
 int main() {
@@ -37,7 +67,7 @@ int main() {
     for (int i = 0; i < n; i++) {
       cin >> arr[i];
     }
-    cout << solution.lenOfLongSubarr_BETTER_APPROACH(arr, n, k) << endl;
+    cout << solution.lenOfLongSubarr_OPTIMAL_APPROACH(arr, n, k) << endl;
   }
 
   return 0;
--- Arrays/Easy/practice.cpp
+++ Arrays/Easy/practice.cpp
@@ -1,158 +1,144 @@
-
-
 #include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
  public:
-  //. Remove Duplicates from an array
-  void remove_duplicates() {
-    int arr[] = {1, 1, 2, 2, 3, 3, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int i = 0;
-    for (int j = 1; j < n; j++) {
-      if (arr[i] != arr[j]) {
-        arr[++i] = arr[j];
-      }
-    }
-    for (int k = 0; k < i; k++) {
-      cout << arr[k] << " ";
-    }
-  }
-
-  // . Left Rotate Array by one number
-
-  void left_rotate_once() {
-    int arr[] = {1, 2, 3, 4, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int temp = arr[0];
-    for (int i = 1; i < n; i++) {
-      arr[i - 1] = arr[i];
-    }
-    arr[n - 1] = temp;
-    for (int i = 0; i < n; i++) cout << arr[i] << " ";
-  }
-
-  //. Check Sorted & Rotated
-
-  void check_sorted_n_rotated() {
-    int arr[] = {2, 3, 4, 5, 1};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int count = 0;
-    for (int i = 0; i < n; i++) {
-      if (arr[n + 1] < (arr[i % n])) count++;
-    }
-    if (count > 1)
-      cout << "True";
-    else
-      cout << "False";
-  }
-
-  // . 2nd Largest Element
-
-  void second_largest_element() {
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 7};
-    int n = sizeof(arr) / sizeof(arr[0]);
+  int print2largest(vector<int>& arr) {
     int largest = arr[0];
     int second_largest = INT_MIN;
-    for (int i = 0; i < n; i++) {
+
+    for (int i = 0; i < arr.size(); i++) {
       if (arr[i] > largest) {
         second_largest = largest;
         largest = arr[i];
-      } else if (arr[i] < largest && arr[i] > second_largest) {
+      }
+
+      else if (arr[i] < largest && arr[i] > second_largest) {
         second_largest = arr[i];
       }
     }
-    cout << second_largest;
+
+    if (second_largest == INT_MIN) {
+      return -1;
+    }
+    return second_largest;
   }
 
-  // . Rotate The array by k places
-  void rotate_array_by_k_places() {
-    int arr[] = {1, 2, 3, 4, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int k = 3;
-    reverse(arr, arr + k);
-    reverse(arr + k, arr + n);
-    reverse(arr, arr + n);
-    for (int i = 0; i < n; i++) cout << arr[i] << " ";
-  }
+  bool check(vector<int>& arr) {
+    int n = arr.size();
+    int count = 0;
 
-  // . Move all zeros to the end
-  void move_all_zeros() {
-    int arr[] = {1, 0, 1, 2, 0, 3, 0, 2};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int j = INT_MIN;
     for (int i = 0; i < n; i++) {
+      if (arr[i] > arr[(i + 1) % n]) {
+        count++;
+      }
+    }
+
+    if (count > 1) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  int removeDuplicates(vector<int>& arr) {
+    int n = arr.size();
+    int i = 0;
+
+    for (int j = 0; j < n; j++) {
+      if (arr[j] != arr[i]) {
+        arr[++i] = arr[j];
+      }
+    }
+
+    return (i + 1);
+  }
+
+  void rotate(vector<int>& arr, int k) {
+    reverse(arr.begin(), arr.end());
+    reverse(arr.begin(), arr.begin() + k);
+    reverse(arr.begin() + k, arr.end());
+  }
+
+  void moveZeroes(vector<int>& arr) {
+    int j = INT_MIN;
+
+    for (int i = 0; i < arr.size(); i++) {
       if (arr[i] == 0) {
         j = i;
         break;
       }
     }
-    if (j == INT_MIN) cout << "No zero element found";
 
-    for (int i = j + 1; i < n; i++) {
+    for (int i = j + 1; i < arr.size(); i++) {
       if (arr[i] != 0) {
         swap(arr[i], arr[j]);
         j++;
       }
     }
-
-    for (int i = 0; i < n; i++) cout << arr[i] << " ";
   }
 
-  // . UNION OF ARRAYS
+  int removeElement(vector<int>& arr, int val) {
+    int j = 0;
+    int n = arr.size();
 
-  void union_of_arrays() {
-    int A[] = {1, 2, 3, 4, 5};
-    int B[] = {2, 3, 4, 5, 6, 7};
-    int n1 = sizeof(A) / sizeof(A[0]);
-    int n2 = sizeof(B) / sizeof(B[0]);
+    for (int i = 0; i < n; i++) {
+      if (arr[i] != val) {
+        arr[j++] = arr[i];
+      }
+    }
+    return j;
+  }
+
+  vector<int> findUnion(vector<int>& A, vector<int>& B, int n1, int n2) {
     int i = 0;
     int j = 0;
-    vector<int> ans;
+    vector<int> union_arr;
 
     while (i < n1 && j < n2) {
       if (A[i] <= B[j]) {
-        if (ans.empty() || ans.back() != A[i]) {
-          ans.push_back(A[i]);
+        if (union_arr.empty() || union_arr.back() != A[i]) {
+          union_arr.push_back(A[i]);
         }
         i++;
-      } else {
-        if (ans.empty() || ans.back() != B[j]) {
-          ans.push_back(B[j]);
+      }
+
+      else {
+        if (union_arr.empty() || union_arr.back() != B[j]) {
+          union_arr.push_back(B[j]);
         }
         j++;
       }
     }
+
     while (i < n1) {
-      if (ans.empty() || ans.back() != A[i]) {
-        ans.push_back(A[i]);
+      if (union_arr.empty() || union_arr.back() != A[i]) {
+        union_arr.push_back(A[i]);
       }
       i++;
     }
+
     while (j < n2) {
-      if (ans.empty() || ans.back() != B[j]) {
-        ans.push_back(B[j]);
+      if (union_arr.empty() || union_arr.back() != B[j]) {
+        union_arr.push_back(B[j]);
       }
       j++;
     }
 
-    for (auto it : ans) cout << it << " ";
+    for (auto it : union_arr) {
+      cout << it << " ";
+    }
+    cout << endl;
   }
 
-  // . INTERSECTION OF ARRAYS
-
-  void intersection_of_array() {
-    int A[] = {1, 2, 3, 4, 5};
-    int B[] = {2, 3, 4, 5, 6, 7};
-    int n1 = sizeof(A) / sizeof(A[0]);
-    int n2 = sizeof(B) / sizeof(B[0]);
+  void intersection(vector<int>& A, vector<int>& B, int n1, int n2) {
     int i = 0;
     int j = 0;
-    vector<int> ans;
-    while (i <= n1 && j <= n2) {
+    vector<int> intersection_arr;
+
+    while (i < n1 && j < n2) {
       if (A[i] == B[j]) {
-        ans.push_back(A[i]);
+        intersection_arr.push_back(A[i]);
         i++;
         j++;
       } else if (A[i] < B[j]) {
@@ -161,129 +147,44 @@ class Solution {
         j++;
       }
     }
-    for (auto it : ans) cout << it << " ";
-  }
 
-  int missingNumber(vector<int>& arr) {
-    int n = arr.size();
-    int sum = (n * (n + 1)) / 2;
-    int s2 = 0;
-    for (int i = 0; i < n; i++) {
-      s2 += arr[i];
+    for (auto it : intersection_arr) {
+      cout << it << " ";
     }
-    return (sum - s2);
+
+    cout << endl;
   }
 
   int findMaxConsecutiveOnes(vector<int>& arr) {
+    int maximum_count = 0;
     int counter = 0;
-    int maxi = 0;
+
     for (int i = 0; i < arr.size(); i++) {
       if (arr[i] == 1) {
         counter++;
-        maxi = max(maxi, counter);
+        maximum_count = max(maximum_count, counter);
       } else {
         counter = 0;
       }
     }
-    return maxi;
+
+    return maximum_count;
   }
 
-  int singleNumber_I(vector<int>& arr) {
-    int n = arr.size();
-    vector<int> hash(n + 1, 0);
-    for (int i = 0; i < n; i++) {
-      hash[arr[i]]++;
-    }
-    for (int i = 0; i < hash.size(); i++) {
-      if (hash[i] == 1) {
-        return i;
-      }
-    }
-    return -1;
-  }
-
-  int singleNumber_II(vector<int>& arr) {
-    int n = arr.size();
+  int singleNumber(vector<int>& arr) {
     unordered_map<int, int> hash;
+
     for (auto it : arr) {
       hash[it]++;
     }
+
     for (auto it : hash) {
       if (it.second == 1) {
         return it.first;
       }
     }
+
     return -1;
-  }
-
-  //. Merge Sort
-  void merge(vector<int>& arr, int low, int mid, int high) {
-    vector<int> temp;
-    int left = low;
-    int right = mid + 1;
-
-    while (left <= mid && right <= high) {
-      if (arr[left] <= arr[right]) {
-        temp.push_back(arr[left]);
-        left++;
-      } else {
-        temp.push_back(arr[right]);
-        right++;
-      }
-    }
-
-    while (left <= mid) {
-      temp.push_back(arr[left]);
-      left++;
-    }
-
-    while (right <= high) {
-      temp.push_back(arr[right]);
-      right++;
-    }
-
-    for (int i = low; i <= high; i++) {
-      arr[i] = temp[i - low];
-    }
-  }
-
-  void mergesort(vector<int>& arr, int low, int high) {
-    if (low < high) {
-      int mid = (low + high) / 2;
-      mergesort(arr, low, mid);
-      mergesort(arr, mid + 1, high);
-      merge(arr, low, mid, high);
-    }
-  }
-
-  //. QUICK SORT
-
-  int partition(vector<int>& arr, int low, int high) {
-    int i = low;
-    int j = high;
-    int pivot = arr[low];
-
-    while (i < j) {
-      while (arr[i] <= pivot && i <= high - 1) {
-        i++;
-      }
-      while (arr[j] > pivot && j >= low + 1) {
-        j--;
-      }
-      if (i < j) {
-        swap(arr[i], arr[j]);
-      }
-    }
-    swap(arr[low], arr[j]);
-    return j;
-  }
-
-  void quicksort(vector<int>& arr, int low, int high) {
-    if (low < high) {
-      int index = partition(arr, low, high);
-      quicksort(arr, low, index - 1);
-      quicksort(arr, index + 1, high);
-    }
   }
 };
 
@@ -299,16 +200,19 @@ int main() {
   while (t--) {
     int n;
     cin >> n;
-    vector<int> arr(n);
-    for (int i = 0; i < n; i++) {
-      cin >> arr[i];
-    }
-    solution.quicksort(arr, 0, n - 1);
+
+    vector<int> A(n);
 
     for (int i = 0; i < n; i++) {
-      cout << arr[i] << " ";
+      cin >> A[i];
     }
-    cout << endl;
+
+    cout << solution.singleNumber(A) << endl;
+    // solution.removeElement(arr, val);
+    // for (int i = 0; i < n; i++) {
+    //   cout << arr[i] << " ";
+    // }
+    // cout << endl;
   }
 
   return 0;
