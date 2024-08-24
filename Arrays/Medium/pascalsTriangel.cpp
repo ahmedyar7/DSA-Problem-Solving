2ab42b443a8d6aa1908cce1fe50b049ae7c4a71c
ahmedyar7 <ahmedyar778@gmail.com>
2024-08-24 11:03:34 +0500

    add

---
+++ Arrays/Medium/pascalsTriangel.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+ public:
+  int subarraySum(vector<int>& nums, int k) {
+    // code here
+  }
+};
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+
+  int t;
+  cin >> t;
+  Solution solution;
+  while (t--) {
+    cout << solution.subarraySum() << endl;
+  }
+
+  return 0;
+}
