ddb4930f50496cad78a00148303d8efe6959655e
ahmedyar7 <ahmedyar778@gmail.com>
2024-08-26 14:33:59 +0500

    adding another file

--- Arrays/Hard/pascalTriangle.cpp
+++ Arrays/Hard/pascalTriangle.cpp
@@ -4,7 +4,15 @@ using namespace std;
 class Solution {
  public:
   vector<vector<int>> generate(int numRows) {
-    // code here
+    vector<vector<int>> result;
+    for (int i = 0; i < numRows; i++) {
+      vector<int> row(i + 1, 1);
+      for (int j = 1; j < i; j++) {
+        row[j] = result[i - 1][j - 1] + result[i - 1][j];
+      }
+      result.push_back(row);
+    }
+    return result;
   }
 };
 
