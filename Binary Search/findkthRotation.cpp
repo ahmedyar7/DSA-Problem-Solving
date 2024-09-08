6f6f64c7116f65feb4b17865204e30bf096534aa
ahmedyar7 <ahmedyar778@gmail.com>
2024-09-08 07:44:59 +0500

    adding another practice file of questions related to the binary serach algorithm

--- Binary Search/findkthRotation.cpp
+++ Binary Search/findkthRotation.cpp
@@ -3,10 +3,35 @@ using namespace std;
 
 class Solution {
  public:
+  // int findKRotation(vector<int> &arr) {
+  //   int low = 0;
+  //   int high = arr.size() - 1;
+  //   int ans = INT_MAX;
+  //   int index = -1;
+
+  //   while (low <= high) {
+  //     int mid = low + (high - low) / 2;
+  //     if (arr[low] <= arr[mid]) {
+  //       if (arr[low] < ans) {
+  //         index = low;
+  //         ans = arr[low];
+  //       }
+  //       low = mid + 1;
+  //     } else {
+  //       if (arr[mid] < ans) {
+  //         index = mid;
+  //         ans = arr[mid];
+  //       }
+  //       high = mid - 1;
+  //     }
+  //   }
+  //   return index;
+  // }
+
   int findKRotation(vector<int> &arr) {
     int low = 0;
     int high = arr.size() - 1;
-    int ans = INT_MAX;
+    int ans = arr.size();
     int index = -1;
 
     while (low <= high) {
--- Binary Search/searchInRotatedArray.cpp
+++ Binary Search/searchInRotatedArray.cpp
@@ -1,37 +1,91 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class Solution {
+//  public:
+// int search(vector<int>& arr, int target) {
+//   int low = 0;
+//   int high = arr.size() - 1;
+
+//   while (low <= high) {
+//     int mid = low + (high - low) / 2;
+//     if (arr[mid] == target) return mid;
+//     if (arr[low] <= arr[mid]) {
+//       if (arr[low] <= target && target <= arr[mid])
+//         high = mid - 1;
+//       else
+//         low = mid + 1;
+//     } else {
+//       if (arr[mid] <= target && target <= arr[high])
+//         low = mid + 1;
+//       else
+//         high = mid - 1;
+//     }
+//   }
+//   return -1;
+// }
+
+//   int search(vector<int>& arr, int target) {
+//     int low = 0;
+//     int high = arr.size() - 1;
+
+//     while (low <= high) {
+//       int mid = low + (high - low) / 2;
+
+//       if (arr[mid] == target) return true;
+//       if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
+//         low++, high--;
+//         continue;
+//       }
+//       if (arr[low] <= arr[mid]) {
+//         if (arr[low] <= target && target <= arr[mid]) {
+//           high = mid - 1;
+//         } else
+//           low = mid + 1;
+//       } else {
+//         if (arr[mid] <= target && target <= arr[high]) {
+//           low = mid + 1;
+//         } else {
+//           high = mid - 1;
+//         }
+//       }
+//     }
+//     return false;
+//   }
+// };
+
+// int main() {
+// #ifndef ONLINE_JUDGE
+//   freopen("input.txt", "r", stdin);
+//   freopen("output.txt", "w", stdout);
+// #endif
+
+//   int t;
+//   cin >> t;
+//   Solution solution;
+//   while (t--) {
+//     int n;
+//     int target;
+//     cin >> n >> target;
+//     vector<int> arr(n);
+
+//     for (int i = 0; i < n; i++) {
+//       cin >> arr[i];
+//     }
+// IIIIIIIIIIIIIiiiiiiicout << solution.search(arr, target) << endl; } return
+// 0; }
+
 #include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
  public:
-  // int search(vector<int>& arr, int target) {
-  //   int low = 0;
-  //   int high = arr.size() - 1;
-
-  //   while (low <= high) {
-  //     int mid = low + (high - low) / 2;
-  //     if (arr[mid] == target) return mid;
-  //     if (arr[low] <= arr[mid]) {
-  //       if (arr[low] <= target && target <= arr[mid])
-  //         high = mid - 1;
-  //       else
-  //         low = mid + 1;
-  //     } else {
-  //       if (arr[mid] <= target && target <= arr[high])
-  //         low = mid + 1;
-  //       else
-  //         high = mid - 1;
-  //     }
-  //   }
-  //   return -1;
-  // }
-
-  int search(vector<int>& arr, int target) {
+  bool search(vector<int>& arr, int target) {
     int low = 0;
     int high = arr.size() - 1;
 
     while (low <= high) {
       int mid = low + (high - low) / 2;
-
       if (arr[mid] == target) return true;
       if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
         low++, high--;
@@ -64,15 +118,10 @@ int main() {
   cin >> t;
   Solution solution;
   while (t--) {
-    int n;
-    int target;
+    int n, target;
     cin >> n >> target;
     vector<int> arr(n);
-
-    for (int i = 0; i < n; i++) {
-      cin >> arr[i];
-    }
-
+    for (int i = 0; i < n; i++) cin >> arr[i];
     cout << solution.search(arr, target) << endl;
   }
 
