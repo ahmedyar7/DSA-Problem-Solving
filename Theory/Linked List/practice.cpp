38fc701645c9f2f6b5ab87846a0528a874f18e8e
ahmedyar7 <ahmedyar778@gmail.com>
2024-09-11 00:14:04 +0500

    practicing questions

--- Theory/Linked List/practice.cpp
+++ Theory/Linked List/practice.cpp
@@ -1,7 +1,7 @@
+#include <climits>
 #include <iostream>
 using namespace std;
 
-// . Creating Linked List
 class Node {
  public:
   int data;
@@ -13,40 +13,49 @@ class Node {
   }
 };
 
-Node* first = NULL;
-
-// . Insert at tail
+//. Inserting Element At Tail
 void insert_at_tail(Node*& head, int value) {
-  Node* n = new Node(value);
+  Node* new_node = new Node(value);
   if (head == NULL) {
-    head = n;
+    head = new_node;
     return;
   }
   Node* temp = head;
   while (temp->next != NULL) {
     temp = temp->next;
   }
-  temp->next = n;
+  temp->next = new_node;
 }
 
-//. Display Function
+//. Display Linked List
 void display(Node* head) {
-  int count = 0;
-  int sum = 0;
   while (head->next != NULL) {
     cout << head->data << " -> ";
-    sum = sum + head->data;
     head = head->next;
-    count++;
   }
   cout << "NULL" << endl;
-  cout << "COUNT: " << count << endl;
-  cout << "SUM: " << sum << endl;
+}
+
+// . Insert at Head
+void insert_at_head(Node*& head, int value) {
+  Node* new_node = new Node(value);
+  new_node->next = head;
+  head = new_node;
+}
+
+// . Count Number of Nodes
+int count_node(Node* head) {
+  int count = 0;
+  while (head->next != NULL) {
+    count++;
+    head = head->next;
+  }
+  cout << "Number of Nodes " << count << endl;
 }
 
 // . Maximum Element
 void maximum_element(Node* head) {
-  int max = -2147483647;
+  int max = INT_MIN;
   while (head->next != NULL) {
     if (head->data > max) {
       max = head->data;
@@ -56,53 +65,46 @@ void maximum_element(Node* head) {
   cout << "Maximum Element " << max << endl;
 }
 
-//. Minimum Element
+// . Minimum Element
 void minimum_element(Node* head) {
-  int min = 2147483647;
+  int min = INT_MAX;
   while (head->next != NULL) {
-    if (head->data < min) min = head->data;
+    if (head->data < min) {
+      min = head->data;
+    }
     head = head->next;
   }
   cout << "Minimum Element " << min << endl;
 }
 
-//. Searching Element
-Node* search(Node*& first, int key) {
-  Node* p = first;
+// . Searching in array
+Node* search(Node* head, int key) {
+  Node* p = head;
   Node* q = NULL;
+
   while (p != NULL) {
     if (p->data == key) {
-      if (q != NULL) {
-        q->next = p->next;
-        p->next = first;
-        first = p;
-      }
       return p;
+      q->next = p->next;
+      p->next = head;
+      head = p;
     }
     q = p;
     p = p->next;
   }
-  return NULL;
-}
-
-// . Inserting at Head
-void before_first_node(Node*& head, int value) {
-  Node* new_node = new Node(value);
-  new_node->next = head;
-  head = new_node;
 }
 
 int main() {
   Node* node = NULL;
   int count = 1;
+
+  insert_at_head(node, 250);
   for (int i = 0; i <= 10; i++) {
     insert_at_tail(node, count);
     count = count * 2;
   }
   display(node);
-  before_first_node(node, 900);
-  display(node);
+  count_node(node);
   maximum_element(node);
   minimum_element(node);
-  cout << "Searching An Element " << search(node, 10) << endl;
 }
