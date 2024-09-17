a07fdc01154d0038c549bb1e7c71096b975d0cdd
ahmedyar7 <ahmedyar778@gmail.com>
2024-09-17 08:08:17 +0500

    adding another qeustion marks

--- Theory/Linked List/practice.cpp
+++ Theory/Linked List/practice.cpp
@@ -1,184 +1,110 @@
 #include <iostream>
 using namespace std;
 
-// . Making the Node
 class Node {
  public:
   int data;
   Node* next;
+
   Node(int value) {
     data = value;
-    next = NULL;
+    next = nullptr;
   }
 };
 
-//. Inserting at tail
 void insert_at_tail(Node*& head, int value) {
   Node* new_node = new Node(value);
-  if (head == NULL) {
+  if (head == nullptr) {
     head = new_node;
-    return;
+    new_node->next = head;
   }
   Node* temp = head;
-  while (temp->next != NULL) {
+  while (temp->next != head) {
     temp = temp->next;
   }
   temp->next = new_node;
-}
-
-//. Total No.of Nodes
-void count_nodes(Node* head) {
-  int counter = 0;
-  while (head != NULL) {
-    counter++;
-    head = head->next;
-  }
-  cout << "No. of Nodes: " << counter << endl;
-}
-
-//. Insert at head
-void insert_at_head(Node*& head, int value) {
-  Node* new_node = new Node(value);
-  Node* p = head;
-  new_node->next = p;
-  head = new_node;
-}
-
-//. Maximum element In LL
-void maximum_element(Node* head) {
-  int max = -1000000;
-  while (head != NULL) {
-    if (head->data > max) {
-      max = head->data;
-    }
-    head = head->next;
-  }
-  cout << "Maximum Element: " << max << endl;
-}
-
-void minimum_element(Node* head) {
-  int min = 1000000;
-  while (head != NULL) {
-    if (head->data < min) {
-      min = head->data;
-    }
-    head = head->next;
-  }
-  cout << "Minimum Element: " << min << endl;
-}
-
-Node* search(Node* head, int key) {
-  Node* p = head;
-  Node* q = NULL;
-  while (head != NULL) {
-    if (p->data == key) {
-      if (q != NULL) {
-        q->next = p->next;
-        p->next = head;
-        head = p;
-      }
-      return p;
-    }
-    return NULL;
-  }
-}
-
-void insert_at_sorted(Node* head, int value) {
-  Node* p = head;
-  Node* q = NULL;
-  Node* new_node = new Node(value);
-  if (head == NULL || head->data >= value) {
-    new_node->next = head;  // New node points to the current head
-    head = new_node;        // Head now points to the new node
-    return;
-  }
-  while (p != NULL && p->data < value) {
-    q = p;
-    p = p->next;
-  }
-  new_node->next = p;
-  q->next = new_node;
-}
-
-void deallocate_memory(Node* head) {
-  Node* current = head;
-  Node* next_node;
-  while (current != NULL) {
-    next_node = current->next;
-    delete current;
-    current = next_node;
-  }
-  head = NULL;
-}
-
-void delete_node(Node*& head, int pos) {
-  if (pos < 1 || head == NULL) return;
-
-  Node* current = head;
-  Node* previous = NULL;
-
-  for (int i = 1; i < pos; i++) {
-    if (current == NULL) return;
-    previous = current;
-    current = current->next;
-  }
-
-  if (previous == NULL) {
-    head = current->next;
-  }
-  previous->next = current->next;
-  delete previous;
+  new_node->next = head;
 }
 
 void display(Node* head) {
-  while (head != NULL) {
-    cout << head->data << " -> ";
-    head = head->next;
-  }
-  cout << "NULL\n";
+  if (head == nullptr) return;
+  Node* temp = head;
+
+  do {
+    cout << temp->data << " -> ";
+    temp = temp->next;
+  } while (temp != head);
+
+  cout << "HEAD\n";
 }
 
-bool check_sorted(Node* head) {
-  int x = -1000000;
-  while (head != NULL) {
-    if (head->data < x) {
-      return false;
-    }
-    x = head->data;
-    head = head->next;
+void insert_at_head(Node*& head, int value) {
+  Node* new_node = new Node(value);
+
+  if (head == nullptr) {
+    head = new_node;
+    new_node->next = head;
   }
-  return true;
+
+  Node* temp = head;
+  while (temp->next != head) {
+    temp = temp->next;
+  }
+
+  new_node->next = head;
+  temp->next = new_node;
+  new_node = head;
 }
 
-void remove_duplicate(Node* head) {
-  if (head == NULL) return;
-  Node* prev = head;
-  Node* current = prev->next;
-  while (current != NULL) {
-    if (prev->data != current->data) {
-      prev = current;
-    } else {
-      prev->next = current->next;
-      delete current;
-    }
-    current = prev->next;
+void insert_at_position(Node*& head, int value, int position) {
+  Node* new_node = new Node(value);
+  if (position < 1 || head == nullptr) {
+    return;
   }
+  if (position == 1) {
+    if (head == nullptr) {
+      insert_at_head(new_node, value);
+    }
+  }
+  Node* temp = head;
+  for (int i = 1; i < position; i++) temp = temp->next;
+  new_node->next = temp->next;
+  temp->next = new_node;
+}
+
+void delete_node(Node*& head, int position) {
+  if (head == nullptr) return;
+
+  if (position == 1) {
+    if (head->next == head) {
+      delete head;
+      head = nullptr;
+      return;
+    }
+  }
+
+  Node* tail = head;
+  while (tail->next != head) tail = tail->next;
+
+  Node* temp = head;
+  head = head->next;
+  tail->next = head;
+  delete temp;
+  return;
+
+  for (int i = 1; i < position && temp->next != head; i++) temp = temp->next;
+  Node* p = temp->next;
+  temp->next = p->next;
+  delete p;
 }
 
 int main() {
-  Node* node = NULL;
-
-  for (int i = 0; i <= 10; i++) {
-    insert_at_tail(node, i);
-  }
-  insert_at_sorted(node, 11);
-  insert_at_head(node, 1010);
-  insert_at_sorted(node, 10010);
-  delete_node(node, 0);
+  Node* node = nullptr;
+  insert_at_head(node, 10);
+  insert_at_position(node, 2, 2);
+  delete_node(node, 2);
+  insert_at_tail(node, 1);
+  insert_at_tail(node, 2);
+  insert_at_tail(node, 3);
   display(node);
-  count_nodes(node);
-  maximum_element(node);
-  minimum_element(node);
-  cout << "Element found: " << search(node, 10010) << endl;
-  deallocate_memory(node);
 }
