fbcaa5a85335dce57e5c98ee7860ac8b33c43bcf
ahmedyar7 <ahmedyar778@gmail.com>
2024-09-17 11:48:34 +0500

    adding another quesiton marks file

--- Theory/Linked List/QUESTIONS.md
+++ Theory/Linked List/QUESTIONS.md
@@ -19,19 +19,23 @@
 - Making them
 - Printing them
 - Inserting element
-- Deleting element
+- Deleting element at first and at any location
 
 ## Doubly Linked List
 
-- Making them
-- Inserting at head
+- Making it
 - Insert at tail
-- Inserting at any position
-- Reversing them
-
-### **NOTE**
-
-**ALL THE OPERATION THAT COULD BE PERFORMED ON SINGLE LINKED LIST COULD ALSO BE DONE WITH DOUBLY LINKED LIST AS WELL**
+- Insert at head
+- Finding Maximum and Minimum
+- Counting total Number of nodes
+- Reversing
+- Searching element
+- Check for cycles
+- Insert at Sorted position
+- Concatenation of 2 linked list
+- Merging of two linked list
+- Check sorted
+- Removing duplicates
 
 ## Circular Doubly Linked List
 
--- Theory/Linked List/practice.cpp
+++ Theory/Linked List/practice.cpp
@@ -5,106 +5,188 @@ class Node {
  public:
   int data;
   Node* next;
-
+  Node* prev;
   Node(int value) {
     data = value;
     next = nullptr;
+    prev = nullptr;
   }
 };
-
-void insert_at_tail(Node*& head, int value) {
-  Node* new_node = new Node(value);
-  if (head == nullptr) {
-    head = new_node;
-    new_node->next = head;
-  }
-  Node* temp = head;
-  while (temp->next != head) {
-    temp = temp->next;
-  }
-  temp->next = new_node;
-  new_node->next = head;
-}
-
-void display(Node* head) {
-  if (head == nullptr) return;
-  Node* temp = head;
-
-  do {
-    cout << temp->data << " -> ";
-    temp = temp->next;
-  } while (temp != head);
-
-  cout << "HEAD\n";
-}
 
 void insert_at_head(Node*& head, int value) {
   Node* new_node = new Node(value);
 
   if (head == nullptr) {
     head = new_node;
-    new_node->next = head;
-  }
-
-  Node* temp = head;
-  while (temp->next != head) {
-    temp = temp->next;
+    return;
   }
 
   new_node->next = head;
-  temp->next = new_node;
-  new_node = head;
+  head->prev = new_node;
+  head = new_node;
+
+  return;
+}
+
+void insert_at_tail(Node*& head, int value) {
+  Node* new_node = new Node(value);
+  Node* current = head;
+
+  if (head == nullptr) {
+    head = new_node;
+    return;
+  }
+
+  while (current->next != nullptr) current = current->next;
+
+  current->next = new_node;
+  new_node->prev = current;
+  new_node->next = nullptr;
+
+  return;
 }
 
 void insert_at_position(Node*& head, int value, int position) {
   Node* new_node = new Node(value);
-  if (position < 1 || head == nullptr) {
-    return;
-  }
-  if (position == 1) {
-    if (head == nullptr) {
-      insert_at_head(new_node, value);
-    }
-  }
-  Node* temp = head;
-  for (int i = 1; i < position; i++) temp = temp->next;
-  new_node->next = temp->next;
-  temp->next = new_node;
+  Node* current = head;
+
+  for (int i = 1; i < position - 1 && current->next != head; i++)
+    current = current->next;
+
+  new_node->next = current->next;
+  new_node->prev = current;
+  current->next = new_node;
+  current->next->prev = new_node;
+
+  return;
 }
 
-void delete_node(Node*& head, int position) {
+void display(Node* head) {
+  Node* current = head;
+  while (current != nullptr) {
+    cout << current->data << " <-> ";
+    current = current->next;
+  }
+  cout << "NULL\n";
+
+  return;
+}
+
+void delete_head_node(Node*& head) {
   if (head == nullptr) return;
 
+  Node* current = head;
+  head = head->next;
+  delete current;
+  head->prev = nullptr;
+
+  return;
+}
+
+void delete_at_position(Node*& head, int position) {
+  if (head == nullptr) return;
+
+  if (position < 1) return;
   if (position == 1) {
-    if (head->next == head) {
-      delete head;
-      head = nullptr;
-      return;
+    delete_head_node(head);
+    return;
+  }
+
+  Node* current = head;
+  for (int i = 1; i < position && current->next != nullptr; i++)
+    current = current->next;
+
+  current->prev->next = current->next;
+  current->next->prev = current->prev;
+
+  delete current;
+  return;
+}
+
+void reverse_dll(Node*& head) {
+  if (head == nullptr) return;
+  Node* current = head;
+  Node* temp = nullptr;
+
+  while (current != nullptr) {
+    temp = current->prev;
+    current->prev = current->next;
+    current->next = temp;
+    current = current->prev;
+  }
+  if (temp != nullptr) head = temp->prev;
+}
+
+Node* concatenation(Node*& node1, Node*& node2) {
+  Node* first = node1;
+  Node* second = node2;
+
+  while (first->next != nullptr) {
+    first = first->next;
+  }
+  first->next = second;
+  second = nullptr;
+  return node1;
+}
+
+Node* merge_doubly_LL(Node*& node1, Node*& node2) {
+  Node* first = node1;
+  Node* second = node2;
+  Node* last = nullptr;
+  Node* third = nullptr;
+
+  if (first == nullptr) return second;
+  if (second == nullptr) return first;
+
+  if (first->data < second->data) {
+    third = last = first;
+    first = first->next;
+  } else {
+    third = last = second;
+    second = second->next;
+  }
+
+  while (first != nullptr && second != nullptr) {
+    if (first->data < second->data) {
+      last->next = first;
+      last = first;
+      first = first->next;
+    } else {
+      last->next = second;
+      last = second;
+      second = second->next;
     }
   }
 
-  Node* tail = head;
-  while (tail->next != head) tail = tail->next;
+  if (first == nullptr)
+    last->next = second;
+  else
+    last->next = first;
 
-  Node* temp = head;
-  head = head->next;
-  tail->next = head;
-  delete temp;
-  return;
-
-  for (int i = 1; i < position && temp->next != head; i++) temp = temp->next;
-  Node* p = temp->next;
-  temp->next = p->next;
-  delete p;
+  return third;
 }
 
 int main() {
-  Node* node = nullptr;
-  insert_at_head(node, 10);
-  insert_at_position(node, 2, 2);
-  delete_node(node, 2);
-  insert_at_tail(node, 1);
-  insert_at_tail(node, 2);
-  insert_at_tail(node, 3);
-  display(node);
+  Node* node1 = nullptr;
+  Node* node2 = nullptr;
+
+  insert_at_tail(node1, 1);
+  insert_at_tail(node1, 3);
+  insert_at_tail(node1, 5);
+  insert_at_tail(node1, 7);
+  insert_at_tail(node1, 9);
+
+  insert_at_tail(node2, 2);
+  insert_at_tail(node2, 4);
+  insert_at_tail(node2, 6);
+  insert_at_tail(node2, 8);
+  insert_at_tail(node2, 10);
+
+  display(node2);
+  display(node1);
+
+  Node* merged = merge_doubly_LL(node1, node2);
+  display(merged);
+
+  return 0;
 }
