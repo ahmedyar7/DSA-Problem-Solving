f6911a6ba48e1cec108e2dcfe36aa3c4fca62fff
ahmedyar7 <ahmedyar778@gmail.com>
2024-09-19 22:56:38 +0500

    adding doubly linked list question in the question bank and marking the question that are important for the this pratice session dated to 19th septemeber 2024 this is very important for my major learning in the computer science field because this is the course of data structurse this will help me in the interview of the course

--- Theory/Linked List/practice.cpp
+++ Theory/Linked List/practice.cpp
@@ -13,98 +13,62 @@ class Node {
   }
 };
 
-void insert_at_head(Node*& head, int value) {
-  Node* new_node = new Node(value);
-
-  if (head == nullptr) {
-    head = new_node;
-    return;
-  }
-
-  new_node->next = head;
-  head->prev = new_node;
-  head = new_node;
-
-  return;
-}
-
 void insert_at_tail(Node*& head, int value) {
   Node* new_node = new Node(value);
-  Node* current = head;
-
   if (head == nullptr) {
     head = new_node;
     return;
   }
 
-  while (current->next != nullptr) current = current->next;
-
-  current->next = new_node;
-  new_node->prev = current;
-  new_node->next = nullptr;
-
-  return;
-}
-
-void insert_at_position(Node*& head, int value, int position) {
-  Node* new_node = new Node(value);
-  Node* current = head;
-
-  for (int i = 1; i < position - 1 && current->next != head; i++)
-    current = current->next;
-
-  new_node->next = current->next;
-  new_node->prev = current;
-  current->next = new_node;
-  current->next->prev = new_node;
-
+  Node* temp = head;
+  while (temp->next != nullptr) {
+    temp = temp->next;
+  }
+  temp->next = new_node;
+  new_node->prev = temp;
   return;
 }
 
 void display(Node* head) {
-  Node* current = head;
-  while (current != nullptr) {
-    cout << current->data << " <-> ";
-    current = current->next;
+  if (head == nullptr) return;
+
+  Node* temp = head;
+  while (temp != nullptr) {
+    cout << temp->data << " <- -> ";
+    temp = temp->next;
   }
   cout << "NULL\n";
-
-  return;
 }
 
-void delete_head_node(Node*& head) {
-  if (head == nullptr) return;
-
-  Node* current = head;
-  head = head->next;
-  delete current;
-  head->prev = nullptr;
-
-  return;
+void insert_at_head(Node*& head, int value) {
+  Node* new_node = new Node(value);
+  if (head == nullptr) {
+    head = new_node;
+  }
+  Node* temp = head;
+  new_node->next = temp;
+  temp->prev = new_node;
+  head = new_node;
 }
 
-void delete_at_position(Node*& head, int position) {
-  if (head == nullptr) return;
-
+void insert_at_position(Node*& head, int value, int position) {
+  Node* new_node = new Node(value);
   if (position < 1) return;
-  if (position == 1) {
-    delete_head_node(head);
-    return;
+  if (head == nullptr || position == 1) {
+    insert_at_head(head, value);
   }
 
-  Node* current = head;
-  for (int i = 1; i < position && current->next != nullptr; i++)
-    current = current->next;
+  Node* temp = head;
+  for (int i = 1; i < position - 1 && temp->next != head; i++) {
+    temp = temp->next;
+  }
 
-  current->prev->next = current->next;
-  current->next->prev = current->prev;
-
-  delete current;
+  new_node->next = temp->next->next;
+  temp->next = new_node;
   return;
 }
 
 void reverse_dll(Node*& head) {
-  if (head == nullptr) return;
   Node* current = head;
   Node* temp = nullptr;
 
@@ -117,90 +81,17 @@ void reverse_dll(Node*& head) {
   if (temp != nullptr) head = temp->prev;
 }
 
-Node* concatenation(Node*& node1, Node*& node2) {
-  Node* first = node1;
-  Node* second = node2;
-
-  while (first->next != nullptr) {
-    first = first->next;
-  }
-  first->next = second;
-  second = nullptr;
-  return node1;
-}
-
-Node* merge_doubly_LL(Node*& node1, Node*& node2) {
-  Node* first = node1;
-  Node* second = node2;
-  Node* last = nullptr;
-  Node* third = nullptr;
-
-  if (first == nullptr) return second;
-  if (second == nullptr) return first;
-
-  if (first->data < second->data) {
-    third = last = first;
-    first = first->next;
-  } else {
-    third = last = second;
-    second = second->next;
-  }
-
-  while (first != nullptr && second != nullptr) {
-    if (first->data < second->data) {
-      last->next = first;
-      last = first;
-      first = first->next;
-    } else {
-      last->next = second;
-      last = second;
-      second = second->next;
-    }
-  }
-
-  if (first == nullptr)
-    last->next = second;
-  else
-    last->next = first;
-  return third;
-}
-
-void remove_element(Node* head, int value) {
-  Node* current = head;
-  Node* prev = nullptr;
-
-  if (head == nullptr || head->next == nullptr) return;
-
-  if (head->data == value) {
-    Node* temp = head;
-    head = head->next;
-    delete temp;
-  }
-
-  while (current != nullptr) {
-    if (current->data == value) {
-      Node* temp = current;
-      prev->next = current->next;
-      current = current->next;
-      delete temp;
-    } else {
-      prev = current;
-      current = current->next;
-    }
-  }
-}
-
 int main() {
   Node* node = nullptr;
-
   insert_at_tail(node, 1);
+  // insert_at_head(node, 2222);
   insert_at_tail(node, 2);
-  insert_at_tail(node, 6);
+  // insert_at_position(node, 2212, 1);
   insert_at_tail(node, 3);
-  insert_at_tail(node, 4);
-  insert_at_tail(node, 5);
+  insert_at_tail(node, 3);
+  insert_at_tail(node, 3);
 
-  remove_element(node, 6);
   display(node);
-  return 0;
+  reverse_dll(node);
+  display(node);
 }
