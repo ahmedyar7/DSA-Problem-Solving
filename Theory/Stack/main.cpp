d3c0e04e5f7c9f0bbf6c0dc1b247839a3dba6008
ahmedyar7 <ahmedyar778@gmail.com>
2024-10-30 11:16:55 +0500

    adding the file for the infix to postfix using stack as arrays

--- Theory/Stack/main.cpp
+++ Theory/Stack/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
+#include <stack>
 
 #include "stack.h"
-
 using namespace std;
 
 int main() {
-  char exp[] = "(a+b)(c-d)";
+  char exp[] = "(a+b)*c/d-e";
   cout << valid_parenthesis(exp) << endl;
   char postfix[100];
   infix_to_postfix(exp, postfix);
