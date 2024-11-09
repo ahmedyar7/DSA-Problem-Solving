d8708a6f443a6652216e8ea4a88f29fbf8a8cd8f
ahmedyar7 <ahmedyar778@gmail.com>
2024-11-09 22:51:48 +0500

    Doublye ciruclar linked list practicining

--- Theory/Linked List/main.cpp
+++ Theory/Linked List/main.cpp
@@ -2,31 +2,33 @@
 
 #include "Node.h"
 #include "circular_ll.h"
+#include "doublycircularll.h"
 #include "doublyll.h"
 using namespace std;
 
 int main() {
-  Doublyll dll;
-  Doublyll dll2;
+  doublycircularll cdll;
 
-  dll.insert_at_tail(0);
-  dll.insert_at_tail(1);
-  dll.insert_at_tail(2);
-  dll.insert_at_tail(3);
-  dll.insert_at_tail(4);
-  dll.insert_at_tail(5);
-  dll.insert_at_tail(5);
-  dll.insert_at_tail(6);
-  dll.insert_at_tail(6);
-  dll.insert_at_tail(7);
-  dll.insert_at_tail(8);
-  dll.insert_at_tail(9);
-  dll.insert_at_tail(9);
-  dll.insert_at_tail(10);
-  dll.insert_at_tail(10);
+  cdll.insert_at_head(1);
+  cdll.insert_at_head(2);
+  cdll.insert_at_head(3);
+  cdll.insert_at_head(4);
+  cdll.insert_at_head(5);
 
-  dll.display();
+  cdll.insert_at_tail(4);
+  cdll.insert_at_tail(5);
+  cdll.insert_at_tail(1);
+  cdll.insert_at_tail(3);
+  cdll.insert_at_tail(2);
 
-  dll.remove_duplicates();
-  dll.display();
+  cdll.display();
+
+  cdll.insert_at_position(999, 3);
+  cdll.display();
+
+  cdll.delete_head();
+  cdll.display();
+
+  cdll.delete_at_position(2);
+  cdll.display();
 }
