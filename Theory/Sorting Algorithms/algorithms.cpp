d5edb02f121e9eb8478953a3f5c5ee7a23d25944
ahmedyar7 <ahmedyar778@gmail.com>
2024-11-16 22:57:38 +0500

    Practicing the Sorting Algorithsm

--- Theory/Sorting Algorithms/algorithms.cpp
+++ Theory/Sorting Algorithms/algorithms.cpp
@@ -5,7 +5,9 @@ void selection_sort(int arr[], int n) {
   for (int i = 0; i <= n - 2; i++) {
     int mini = i;
     for (int j = i; j <= n - 1; j++) {
-      if (arr[j] < arr[mini]) mini = j;
+      if (arr[j] < arr[mini]) {
+        mini = j;
+      }
     }
     int temp = arr[mini];
     arr[mini] = arr[i];
@@ -16,9 +18,10 @@ void selection_sort(int arr[], int n) {
 void print_arr(int arr[], int n) {
   cout << "[";
   for (int i = 0; i < n; i++) {
-    cout << "" << arr[i] << "" << " ";
+    cout << "" << arr[i] << " ";
   }
   cout << "]";
+  cout << endl;
 }
 
 void bubble_sort(int arr[], int n) {
@@ -32,8 +35,28 @@ void bubble_sort(int arr[], int n) {
         did_swap = true;
       }
     }
-    if (did_swap == false) {
+    if (!did_swap) {  // If no swaps were made, the array is sorted
       break;
     }
   }
 }
+
+void insertion_sort(int arr[], int n) {
+  for (int i = 0; i <= n - 1; i++) {
+    int j = i;
+    while (j > 0 && arr[j - 1] > arr[j]) {
+      int temp = arr[j - 1];
+      arr[j - 1] = arr[j];
+      arr[j] = temp;
+      j--;
+    }
+  }
+}
+
+void merge(int arr[], int low, int mid, int high) {}
+
+void merge_sort(int arr[], int low, int high) {}
+
+int partition(int arr[], int low, int high) {}
+
+void quick_sort(int arr[], int low, int high) {}
--- Theory/Sorting Algorithms/main.cpp
+++ Theory/Sorting Algorithms/main.cpp
@@ -4,10 +4,21 @@
 using namespace std;
 
 int main() {
-  int arr[] = {13, 46, 24, 52, 20, 9};
+  int arr[] = {23, 12, 1, 54, 3, 0, 55, 11, 44, 233, 23111, 44, 343};
   int n = sizeof(arr) / sizeof(arr[0]);
 
-  selection_sort(arr, n);
-  // bubble_sort(arr, n);
+  // selection_sort(arr, n);
+  // print_arr(arr, n);
+
+  insertion_sort(arr, n);
   print_arr(arr, n);
+
+  // bubble_sort(arr, n);
+  // print_arr(arr, n);
+
+  // merge_sort(arr, 0, n - 1);
+  // print_arr(arr, n);
+
+  // quick_sort(arr, 0, n - 1);
+  // print_arr(arr, n);
 }
