b66b5c302e3329ae3e687736e9a53bec20511629
ahmedyar7 <ahmedyar778@gmail.com>
2024-12-02 19:09:56 +0500

    Adding Practice to AVL trees

--- Theory/AVL Trees/main.cpp
+++ Theory/AVL Trees/main.cpp
@@ -4,26 +4,20 @@
 using namespace std;
 
 int main() {
-  AVLTrees avlTree;
+  AVLTrees tree;
+  tree.insert(10);
+  tree.insert(20);
+  tree.insert(30);
+  tree.insert(25);
+  tree.insert(5);
+  tree.insert(35);
 
-  cout << "Inserting values into the AVL Tree..." << endl;
-  avlTree.insert(10);
-  avlTree.insert(20);
-  avlTree.insert(30);
-  avlTree.insert(40);
-  avlTree.insert(50);
-  avlTree.insert(25);
+  cout << "Inorder traversal of AVL tree:" << endl;
+  tree.inorder();
 
-  cout << "In-order traversal of the AVL Tree after insertion:" << endl;
-  avlTree.inorder();  // Expected: 10 20 25 30 40 50
-  cout << endl;
-
-  cout << "Deleting value 40 from the AVL Tree..." << endl;
-  avlTree.delete_node(avlTree.root, 40);
-
-  cout << "In-order traversal of the AVL Tree after deletion:" << endl;
-  avlTree.inorder();  // Expected: 10 20 25 30 50
-  cout << endl;
+  tree.delete_value(10);
+  cout << "Inorder after deleting 10:" << endl;
+  tree.inorder();
 
   return 0;
 }
